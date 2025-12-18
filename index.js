824d943d59f59cc36cf12c6a86c856a5d8f3e80b
Mitchelle-git <mitchmarios2@gmail.com>
2025-12-18 11:33:42 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+require('datejs');
 
+const combineUsers = (...args) => {
+  const combinedObject = {
+    users: [],
+
+  merge_date:new Date().toString('M/d/yyyy')
+  }
+
+  args.forEach(internalArray => {
+    combinedObject.users.push(...internalArray);
+  });
+
+return combinedObject;
+}
 
 
 module.exports = {
